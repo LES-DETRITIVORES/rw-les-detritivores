66d9fa4fbd5b6d1484ffc4f29f1a991d6e972581
Do HUYNH <admin@les-detritivores.co>
2022-06-15 11:57:34 +0200

    set default body background color

--- web/src/pages/ManifestPage/ManifestPage.js
+++ web/src/pages/ManifestPage/ManifestPage.js
@@ -9,7 +9,7 @@ const ManifestPage = () => {
       <MetaTags title="Manifest" description="Manifest page" />
       <a href="https://les-detritivores.co"><img className="w-72 p-6 mx-auto" src={logoUrl} /></a>
 
-      <div className="font-sans text-white text-6xl font-bold inline-block mx-24 my-6 space-y-12 leading-tight">
+      <div className="font-sans text-white text-3xl md:text-6xl font-bold inline-block mx-12 md:mx-24 my-6 space-y-12 leading-tight">
         <p>
           Nous sommes <span className="bg-orange-600">LES&nbsp;DETRITIVORES</span>.
         </p>
@@ -19,17 +19,17 @@ const ManifestPage = () => {
           pour en faire un <span className="bg-orange-600">compost</span> nutritif et local.
         </p>
         <p>
-          Mais plus que ça, nous voulons prendre soin chaque jour de la <span className="bg-orange-600">vie humaine</span> et <span className="bg-orange-600">organique</span>.
-          C'est pourquoi nous créons des <span className="bg-orange-600">emplois</span> proches, utiles et durables au coeur des <span className="bg-orange-600">territoires</span> urbains et ruraux.
+          Et au-delà de la <span className="bg-orange-600">vie organique</span>, nous voulons également prendre soin de la <span className="bg-orange-600">vie humaine</span>.
+          C'est pourquoi nous créons des <span className="bg-orange-600">emplois</span> formateurs, utiles et durables au coeur des <span className="bg-orange-600">territoires</span> urbains et ruraux.
         </p>
         <p>
         <span className="bg-orange-600">Citoyens</span>, <span className="bg-orange-600">professionnels</span>, <span className="bg-orange-600">collectivités</span>, nous pouvons tout changer ensemble. 
         </p>
         <p>
-          Face à l'épuisement, régénérons la <span className="bg-orange-600">terre</span>.
-          Face à l'exclusion, accompagnons vers le <span className="bg-orange-600">respect</span>.
-          Face au profit, défendons des <span className="bg-orange-600">valeurs</span>.
-          Face au monopole, partageons les <span className="bg-orange-600">ressources</span>.
+          Face à l'épuisement, régénérons les <span className="bg-orange-600">sols</span>.
+          Face aux exclusions, accompagnons vers le <span className="bg-orange-600">respect</span>.
+          Face aux profits, cultivons des <span className="bg-orange-600">valeurs</span>.
+          Face aux monopoles, partageons nos <span className="bg-orange-600">ressources</span>.
         </p>
         <p>          
           Rejoignez notre mouvement pour des territoires <span className="bg-orange-600">organiques</span>, <span className="bg-orange-600">inclusifs</span> et <span className="bg-orange-600">coopératifs</span> !
