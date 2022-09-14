fe451ada98adffe437a980b69b026889f6c052a2
Do HUYNH <admin@les-detritivores.co>
2022-09-14 18:36:14 +0200

    Update cooperation page

--- web/src/pages/CooperationPage/CooperationPage.js
+++ web/src/pages/CooperationPage/CooperationPage.js
@@ -3,61 +3,53 @@ import { MetaTags } from '@redwoodjs/web'
 import { IoHandLeft } from "react-icons/io5"
 
 const CooperationPage = () => {
-  const groupUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_211007_678_0j3dm7hjRI.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651843629208"
-  const teamUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_220516_0516_MgiSISfUR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655423311749"
+  const teamUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_211007_678_0j3dm7hjRI.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651843629208"
+  const userUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_211202_599__s4xTKpKa.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651844829335"
+  const cycleUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_211202_799_m6brY_eePD.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1651843538772"
 
   return (
     <>
-      <MetaTags title="Collect" description="Collect page" />
-      <h1 className="text-md md:text-lg text-green-900 font-semibold mb-6"><IoHandLeft className="inline mr-2 align-middle h-6 w-6"/>Vie coopérative > Société Coopérative d'Intérêt Collectif (SCIC)</h1>
-      <div className="flex flex-col md:flex-row gap-6 mb-6">
+      <MetaTags title="Société Coopérative d'Intérêt Collectif (SCIC)" description="Notre projet coopératif rassemble les particuliers, les professionnels, les financeurs, les institutions, les collectivités autour d'un projet commun de transition organique, inclusive et coopérative des territoires." />
+      <div className="font-light">&lt; <a href="/" className="underline">retour accueil</a></div>
+      <h1 className="text-md md:text-lg text-green-900 font-semibold mt-3 mb-6">
+        <div>
+          <IoHandLeft className="inline mr-2 align-middle h-6 w-6"/>Société Coopérative d'Intérêt Collectif (SCIC)
+        </div>
+      </h1>
+      <div className="flex flex-col gap-6 mb-8 md:flex-row md:gap-24 md:mb-24">
         <div className="basis-full md:basis-1/2">
-          <h2 className="text-xl md:text-3xl text-black font-bold">Tout seul on va plus vite mais ensemble on va plus loin !</h2>
+          <h2 className="text-xl md:text-3xl text-black font-bold">Ensemble, allons plus loin&nbsp;!</h2>
           <div className="my-3 space-y-3 text-sm md:text-md lg:text-lg font-light">
             <p>
               En 2019, une nouvelle page de LES DETRITIVORES a commencé à s’écrire : 
-              nous avons quitté le monde associatif auquel nous étions attachés depuis notre création en 2015 et sommes devenus une SCIC.
+              nous avons quitté le monde associatif auquel nous étions attachés depuis notre création en 2015 et sommes devenus une <span className="font-bold">SCIC : Société Coopérative d'Intérêt Collectif</span>.
+            </p>
+            <p className="font-extralight px-6">
+              « Les Sociétés Coopératives d'Intérêt Collectif ont pour objet la <span className="font-bold">production ou la fourniture de biens et de services d’intérêt collectif</span> qui présentent un caractère d’<span className="font-bold">utilité sociale</span>.» <span className="align-right italic text-sm">- Loi n°2001-624 du 17 juillet 2001</span> 
             </p>
             <p>
-              « Les Sociétés Coopératives à Intérêt Collectif ont pour objet la production ou la fourniture de biens et de services d’intérêt collectif qui présentent un caractère d’utilité sociale » Loi n°2001-624 du 17 juillet 2001. 
-              Elles encouragent un meilleur partage de la Gouvernance ainsi qu’une répartition plus juste des bénéfices : au moins 57,5% des bénéfices doivent être réinjectés dans l’entreprise et les sociétaires ont le pouvoir de porter ce taux à 100%.
+              Elles encouragent un <span className="font-bold">meilleur partage de la gouvernance</span> ainsi qu’une <span className="font-bold">répartition plus juste des bénéfices</span> : au moins 57,5% des bénéfices doivent être réinjectés dans l’entreprise et les sociétaires ont le pouvoir de porter ce taux à 100%.
             </p>
-            Ces objectifs principaux sont :
-            <ul className="space-y-3">
-              <li>
-                <span className="font-bold">Mobiliser</span> les acteurs autour d’un projet commun
-              </li>
-              <li>
-                <span className="font-bold">Valoriser</span> la coopération entre les partie-prenantes
-              </li>
-              <li>
-                <span className="font-bold">Participer</span> au développement du Territoire
-              </li>
-              <li>
-                <span className="font-bold">Agir</span> pour le plus grand nombre
-              </li>
-            </ul>
           </div>
         </div>
-        <div className="my-auto basis-full md:basis-1/2">
-          <img className="w-full md:w-96 md:m-auto" src={groupUrl}></img>
+        <div className="basis-full rounded-2xl md:h-auto md:basis-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${teamUrl})` }}>
+          <div className="h-64 md:h-full"></div>
         </div>
       </div>
-      <div className="flex flex-col-reverse md:flex-row gap-6 mb-6">
-        <div className="my-auto basis-full md:basis-1/2">
-          <img className="w-full md:w-96 md:m-auto" src={teamUrl}></img>
+      <div className="flex flex-col-reverse gap-6 mb-8 md:flex-row md:gap-24 md:mb-24">
+        <div className="basis-full rounded-2xl md:h-auto md:basis-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${userUrl})` }}>
+          <div className="h-64 md:h-full"></div>
         </div>
         <div className="basis-full md:basis-1/2">
-          <h2 className="text-xl md:text-3xl text-black font-bold">Devenez acteur de la transition.</h2>
+          <h2 className="text-xl md:text-3xl text-black font-bold">Prenez part à la transition.</h2>
           <div className="my-3 space-y-3 text-sm md:text-md lg:text-lg font-light">
             <p>
-              En tant qu’Entreprise d’Insertion et coopérative d’intérêt collectif, nous œuvrons au quotidien 
-              pour que notre activité de collecte et de valorisation des biodéchets soit une réelle plus-value 
-              pour notre territoire et ses acteurs. 
-              Dans cet objectif, nous vous invitons à participer à la vie de notre coopérative en devenant sociétaire !
+              Nous œuvrons au quotidien pour que notre activité de collecte et de valorisation des biodéchets soit <span className="font-bold">une réelle plus-value 
+              pour notre territoire et ses acteurs</span>. 
+              Dans cet objectif, nous vous invitons à participer à la vie de notre coopérative en devenant <span className="font-bold">sociétaire</span> !
             </p>
             <p>
-              Nous proposons à nos clients de s’engager en tant que sociétaire dans notre coopérative en prenant au minimum une part sociale.
+              Nous proposons également à l'ensemble de nos clients-usagers de s’engager en tant que sociétaire dans notre coopérative en prenant au minimum une <span className="font-bold">part sociale</span>.
             </p>
           </div>
         </div>
--- web/src/pages/IntegrationPage/IntegrationPage.js
+++ web/src/pages/IntegrationPage/IntegrationPage.js
@@ -3,15 +3,21 @@ import { MetaTags } from '@redwoodjs/web'
 import { IoFootsteps } from "react-icons/io5"
 
 const IntegrationPage = () => {
+  const workUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_220516_0516_MgiSISfUR.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655423311749"
   const inclusionUrl = "https://ik.imagekit.io/dttv/SHOOTING/DETRI_220516_0006_KhJB0AFwQ.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1655423300412"
 
   return (
     <>
-      <MetaTags title="Collect" description="Collect page" />
-      <h1 className="text-md md:text-lg text-green-900 font-semibold mb-6"><IoFootsteps className="inline mr-2 align-middle h-6 w-6"/>Engagements inclusifs > Entreprise d'insertion</h1>
-      <div className="flex flex-col md:flex-row gap-6 mb-6">
+      <MetaTags title="Entreprise d'insertion" description="Nous recrutons en priorité des personnes éloignées de l'emploi qui bénéficient tout au long de leur parcours d'un accompagnement professionnel sur mesure." />
+      <div className="font-light">&lt; <a href="/" className="underline">retour accueil</a></div>
+      <h1 className="text-md md:text-lg text-green-900 font-semibold mt-3 mb-6">
+        <div>
+        <IoFootsteps className="inline mr-2 align-middle h-6 w-6"/>Entreprise d'insertion
+        </div>
+      </h1>
+      <div className="flex flex-col gap-6 mb-8 md:flex-row md:gap-24 md:mb-24">
         <div className="basis-full md:basis-1/2">
-          <h2 className="text-xl md:text-3xl text-black font-bold">Quand le compost crée des postes... d'insertion !</h2>
+          <h2 className="text-xl md:text-3xl text-black font-bold">Un travail à la mesure de chacun.</h2>
           <div className="my-3 space-y-3 text-sm md:text-md lg:text-lg font-light">
             <p>
               En tant qu’Entreprise d’Insertion, nous nous engageons à favoriser l’acquisition par nos salariés de nouvelles compétences. 
@@ -22,24 +28,37 @@ const IntegrationPage = () => {
               Nous veillons à accompagner nos salariés dans la co-construction de leur projet professionnel 
               grâce à la mise en place de parcours professionnels durables et répondant à leurs besoins.
             </p>
+          </div>
+        </div>
+        <div className="basis-full rounded-2xl md:h-auto md:basis-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${workUrl})` }}>
+          <div className="h-64 md:h-full"></div>
+        </div>
+      </div>
+      <div className="flex flex-col-reverse gap-6 mb-8 md:flex-row md:gap-24 md:mb-24">
+        <div className="basis-full rounded-2xl md:h-auto md:basis-1/2 bg-cover bg-center" style={{ backgroundImage: `url(${inclusionUrl})` }}>
+          <div className="h-64 md:h-full"></div>
+        </div>
+        <div className="basis-full md:basis-1/2">
+          <h2 className="text-xl md:text-3xl text-black font-bold">Se tenir debout.</h2>
+          <div className="my-3 space-y-3 text-sm md:text-md lg:text-lg font-light">
+            <p>
+              Notre coopérative s'appuie sur un socle de missions sociales fortes qui défend la dignité et la liberté des personnes accompagnées :
+            </p>
             <ul className="space-y-3">
               <li>
-                <span className="font-bold">Utilité sociale :</span>S’engager dans une activité à forte utilité sociale en créeant des emplois inclusifs
+                <span className="font-bold block">Utilité sociale</span> S’engager dans une activité à forte utilité sociale en créant des emplois inclusifs
               </li>
               <li>
-                <span className="font-bold">Insertion :</span> Favoriser la réinsertion de personnes rencontrant des difficultés professionnelles et sociales
+                <span className="font-bold block">Insertion</span> Favoriser la réinsertion de personnes rencontrant des difficultés professionnelles et sociales
               </li>
               <li>
-                <span className="font-bold">Accompagnement :</span> Permettre une meilleure employabilité grâce à un accompagnement socio-professionnel
+                <span className="font-bold block">Accompagnement</span> Permettre une meilleure employabilité grâce à un accompagnement socio-professionnel
               </li>
               <li>
-                <span className="font-bold">Autonomie :</span> Favoriser l'autonomie professionnelle des individus que nous soutenons.
+                <span className="font-bold block">Autonomie</span> Favoriser l'autonomie professionnelle des individus que nous soutenons.
               </li>
             </ul>
           </div>
-        </div>
-        <div className="my-auto basis-full md:basis-1/2">
-          <img className="w-full md:w-96 md:m-auto" src={inclusionUrl}></img>
         </div>
       </div>
     </>
